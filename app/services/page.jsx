10e5b2f453c7142027cc948e585f1b4b2e1d2517
Godwin <nwfrgodwin@gmail.com>
2024-06-17 15:51:40 +0100

    Completed Services & Milestone:feat

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,14 @@
 import Layout from "../components/shared/navigation/Layout";
+import Services from "../components/pages/Services";
 
 const Service = () => {
-  return <Layout>Services</Layout>;
+  return (
+    <Layout>
+      <div className="px-10 mt-24">
+        <Services />
+      </div>
+    </Layout>
+  );
 };
 
 export default Service;
